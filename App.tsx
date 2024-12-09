6646df3848fde05cb2264612ff6626999e71f6cd
Oleg Brezhnev <oleg-423@yandex.ru>
2024-12-09 17:14:46 +0300

    AwesomeProject

--- App.tsx
+++ App.tsx
@@ -1,118 +1,144 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import { useState, useEffect } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Button,
+  TextInput,
+  Text,
+  NativeEventEmitter,
+  NativeModules,
 } from 'react-native';
-
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  startService,
+  stopService,
+  PushedEventTypes,
+  Push,
+} from 'pushed-react-native';
+import notifee from '@notifee/react-native';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+export default function App() {
+  const [serviceStarted, setServiceStarted] = useState(false);
+  const [token, setToken] = useState('');
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  async function onDisplayNotification(push: Push) {
+    // Request permissions (required for iOS)
+    await notifee.requestPermission()
+
+    // Create a channel (required for Android)
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      name: 'Default Channel',
+    });
+
+    // Display a notification
+    await notifee.displayNotification({
+      title: 'Notification Title',
+      body: 'Main body content of the notification',
+      android: {
+        channelId,
+        smallIcon: 'name-of-a-small-icon', // optional, defaults to 'ic_launcher'.
+        // pressAction is needed if you want the notification to open the app when pressed
+        pressAction: {
+          id: 'default',
+        },
+      },
+    });
+  }
+
+  const handleStart = () => {
+    console.log('Starting Pushed Service');
+    startService('PushedService').then((newToken) => {
+      console.log(`Service has started: ${newToken}`);
+      setToken(newToken);
+      setServiceStarted(true);
+    });
+  };
+
+  const handleStop = () => {
+    stopService().then((message) => {
+      console.log(message);
+      setToken('');
+      setServiceStarted(false);
+    });
+  };
+
+  useEffect(() => {
+    const eventEmitter = new NativeEventEmitter(
+      NativeModules.PushedReactNative
+    );
+    const eventListener = eventEmitter.addListener(
+      PushedEventTypes.PUSH_RECEIVED,
+      (push: Push) => {
+        console.log(push);
+        onDisplayNotification(push);
+      }
+    );
+
+    // Removes the listener once unmounted
+    return () => {
+      eventListener.remove();
+    };
+  }, []);
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      {serviceStarted ? (
+        <>
+          <Text style={styles.label}>Token:</Text>
+          <TextInput
+            style={styles.textInput}
+            value={token}
+            editable={false}
+            selectTextOnFocus={true}
+          />
+        </>
+      ) : (
+        <Text style={styles.notListeningLabel}>Service is not listening</Text>
+      )}
+      <View style={styles.buttonRow}>
+        <Button title="Start" onPress={handleStart} disabled={serviceStarted} />
+        <Button title="Stop" onPress={handleStop} disabled={!serviceStarted} />
+      </View>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    gap: 2,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  label: {
+    marginBottom: 10,
+    fontSize: 16,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    width: '90%',
+    textAlign: 'center',
   },
-  highlight: {
-    fontWeight: '700',
+  notListeningLabel: {
+    marginBottom: 20,
+    fontSize: 16,
+    color: 'red',
+  },
+  buttonRow: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    gap: 16,
+    width: '80%',
+  },
+  button: {
+    flex: 1,
+    marginHorizontal: 5,
   },
 });
-
-export default App;
